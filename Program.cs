0a0ff91820cfc2b677fa6b0cd5d4eef60c49389e
Guy Siedes <3strategy@gmail.com>
2022-09-11 10:54:44 +0300

    Added Frog

--- Program.cs
+++ Program.cs
@@ -11,20 +11,19 @@ namespace Turtle22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you want to run \nmain 1 (Turtle) or \n2 (Bucket)?\n\n\n\t\ttype 1 or 2");
+            Console.WriteLine("Do you want to run \nmain 1 (Turtle) or \n2 (Bucket)?\n3 (Frog)?" +
+                "\n\n\n\t\ttype 1, 2, or 3");
             int n = int.Parse(Console.ReadLine());
             if (n == 1)
             {
                 Main1();
             }
             else if (n == 2)
-            {
                 Main2();
-            }
+            else if (n == 3)
+                Main3();
             else
-            {
                 Console.WriteLine("You selected an invalid option");
-            }
         }
 
 
---
+++ Program3Frog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.FrogLib;
+
+namespace Turtle22
+{
+    internal partial class Program
+    {
+        static void Main3()
+        {
+            BrownFrog f0 = new BrownFrog(0, "0");
+            BrownFrog f1 = new BrownFrog(1, "1");
+            BrownFrog f2 = new BrownFrog(2, "2");
+
+            GreenFrog f3 = new GreenFrog(4, "4"); 
+            GreenFrog f5 = new GreenFrog(5, "5"); 
+            GreenFrog f6 = new GreenFrog(6, "6");
+
+        }
+    }
+}
