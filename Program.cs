1942accde3183f4e494f5978f5407e1bcf3cb854
Guy Siedes <3strategy@gmail.com>
2022-09-11 08:33:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.TurtleLib;
+
+namespace Turtle22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle t1 = new Turtle();
+
+            //t1.MoveBackward(200);
+            t1.SetDelay(200);
+            t1.TailDown();
+
+            t1.MoveForward(50);
+            t1.TurnRight(90);
+            t1.MoveForward(100);
+            t1.TurnLeft(90);
+
+            t1.MoveForward(50);
+            t1.TurnRight(90);
+            t1.MoveForward(100);
+            t1.TurnLeft(90);
+        }
+    }
+}
